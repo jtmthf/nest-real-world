f61a180f47508c9e12f584605a4eeb7f0d2c2d13
Jack Moore <8162598+jtmthf@users.noreply.github.com>
2024-09-27 14:31:17 -0500

    pino

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    {
+      bufferLogs: true,
+    },
   );
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
